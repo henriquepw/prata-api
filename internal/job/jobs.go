07a12b0918eb2eb6473425a6ee11de0f0fa636c7
Felipe Ferreira <felipeffbs3x@gmail.com>
2025-01-12 14:16:01 -0300

    moved to a proper package

---
+++ internal/job/jobs.go
@@ -0,0 +1,53 @@
+package job
+
+import (
+	"log"
+
+	"github.com/go-co-op/gocron/v2"
+	"golang.org/x/exp/slog"
+)
+
+const (
+	CronEverydayAtMidnight = "0 0 * * *"
+	CronEveryHour          = "0 * * * *"
+)
+
+type jobServer struct {
+	scheduler gocron.Scheduler
+	running   map[string]struct{}
+}
+
+func NewServer() (*jobServer, error) {
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		return nil, err
+	}
+
+	return &jobServer{
+		scheduler: s,
+		running:   map[string]struct{}{},
+	}, nil
+}
+
+func (s *jobServer) runTask(cron, name string, task func()) error {
+	_, err := s.scheduler.NewJob(gocron.CronJob(cron, false), gocron.NewTask(func() {
+		_, ok := s.running[name]
+		if ok {
+			return
+		}
+
+		s.running[name] = struct{}{}
+		task()
+		delete(s.running, name)
+	}), gocron.WithName(name))
+	if err != nil {
+		slog.Error("can't start job", "name", name)
+	}
+
+	return err
+}
+
+func (s *jobServer) Start() error {
+	log.Print("TODO JOB")
+	return nil
+}
