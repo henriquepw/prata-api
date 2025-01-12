7e140b9d29ce3701cb5cbd9d4010b661528f1a04
Felipe Ferreira <felipeffbs3x@gmail.com>
2025-01-12 14:12:58 -0300

    using singleton

---
+++ internal/database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"log"
+	"sync"
+
+	"github.com/henriquepw/pobrin-api/internal/config"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/tursodatabase/go-libsql"
+)
+
+var (
+	dbConn  *sqlx.DB
+	dbMutex = &sync.Mutex{}
+)
+
+func GetDBConnection() (*sqlx.DB, error) {
+	if dbConn == nil {
+		dbMutex.Lock()
+		defer dbMutex.Unlock()
+
+		if dbConn == nil {
+			db, err := startDB()
+			if err != nil {
+				return nil, err
+			}
+
+			dbConn = db
+		}
+	}
+
+	return dbConn, nil
+}
+
+func startDB() (*sqlx.DB, error) {
+	db, err := sqlx.Open("libsql", config.Env().DatabaseURL)
+	if err != nil {
+		log.Fatalf("failed to open db: %s", err.Error())
+
+		return nil, err
+	}
+
+	return db, nil
+}
