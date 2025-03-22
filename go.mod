0ee825c99b79ef0241f2363aac55bcb710c77cb5
Henrique Miranda <henriquepw4@gmail.com>
2025-03-22 13:54:48 -0300

    config job file

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-co-op/gocron/v2 v2.12.3
 	github.com/go-playground/validator/v10 v10.22.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/joho/godotenv v1.5.1
 	github.com/nrednav/cuid2 v1.0.1
@@ -22,7 +23,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/libsql/sqlite-antlr4-parser v0.0.0-20240327125255-dbf53b6cbf06 // indirect
--- internal/job/jobs.go
+++ internal/job/jobs.go
@@ -3,13 +3,15 @@ package job
 import (
 	"log"
 	"log/slog"
+	"time"
 
 	"github.com/go-co-op/gocron/v2"
+	"github.com/google/uuid"
 )
 
 const (
-	CronEverydayAtMidnight = "0 0 * * *"
-	CronEveryHour          = "0 * * * *"
+	CronEveryday  = "0 0 * * *"
+	CronEveryHour = "0 * * * *"
 )
 
 type jobServer struct {
@@ -18,14 +20,32 @@ type jobServer struct {
 }
 
 func New() *jobServer {
-	s, _ := gocron.NewScheduler()
+	s, err := gocron.NewScheduler(
+		gocron.WithStopTimeout(3*time.Hour),
+		gocron.WithGlobalJobOptions(
+			gocron.WithEventListeners(
+				gocron.BeforeJobRuns(func(jobID uuid.UUID, jobName string) {
+					slog.Info("[JOB] Starting:", "jobID", jobID, "jobName", jobName)
+				}),
+				gocron.AfterJobRuns(func(jobID uuid.UUID, jobName string) {
+					slog.Info("[JOB] Finished:", "jobID", jobID, "jobName", jobName)
+				}),
+				gocron.AfterJobRunsWithError(func(jobID uuid.UUID, jobName string, err error) {
+					slog.Info("[JOB] Finished with error:", "jobID", jobID, "jobName", jobName, "error", err.Error())
+				}),
+			),
+		))
+	if err != nil {
+		panic(err)
+	}
+
 	return &jobServer{
 		scheduler: s,
 		running:   map[string]struct{}{},
 	}
 }
 
-func (s *jobServer) runTask(cron, name string, task func()) error {
+func (s *jobServer) addTask(cron, name string, task func()) {
 	_, err := s.scheduler.NewJob(gocron.CronJob(cron, false), gocron.NewTask(func() {
 		_, ok := s.running[name]
 		if ok {
@@ -38,12 +58,14 @@ func (s *jobServer) runTask(cron, name string, task func()) error {
 	}), gocron.WithName(name))
 	if err != nil {
 		slog.Error("can't start job", "name", name)
+		panic(err)
 	}
-
-	return err
 }
 
 func (s *jobServer) Start() error {
 	log.Print("TODO JOB")
+
+	s.addTask(CronEveryday, "create-transactions-by-transactions", createTransactionByRecurrence)
+
 	return nil
 }
